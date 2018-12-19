26888071964c57018b043213b5193d1f3641d002
sddivid <sduum82@gmail.com>
2018-12-19 16:28:44 +0800

    專案核心基礎 功能：Firebase 與 ＲＮTAB頁面切換 使用   react-navigation:2.8.1 react-native-firebase:5.1.1

--- App.1.js
+++ App.1.js
@@ -1,32 +1,25 @@
 import React from 'react';
-import { createBottomTabNavigator } from 'react-navigation';
-import { StyleSheet, Platform, Image, Button, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+
 import firebase from 'react-native-firebase';
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
   }
-}
 
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
+  async componentDidMount() {
+    // TODO: You: Do firebase things
+    // const { user } = await firebase.auth().signInAnonymously();
+    // console.warn('User -> ', user.toJSON());
+
+    // await firebase.analytics().logEvent('foo', { bar: '123'});
   }
-}
-class meow extends React.Component {
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-<ScrollView>
+      <ScrollView>
         <View style={styles.container}>
           <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
           <Text style={styles.welcome}>
@@ -65,17 +58,10 @@ class meow extends React.Component {
             {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
           </View>
         </View>
-      </ScrollView>        
-        </View>
+      </ScrollView>
     );
   }
 }
-export default createBottomTabNavigator({
-  Home: { screen: HomeScreen },
-  Settings: { screen: SettingsScreen },
-  meow: { screen: meow },
-});
-
 
 const styles = StyleSheet.create({
   container: {
